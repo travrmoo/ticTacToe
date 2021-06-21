a795dcd9de6f3330500f40ae4c2fb989ecb10923
travrmoo <travrmoo@gmail.com>
2021-06-21 13:40:24 -0500

    Initial Commit

---
+++ ticTacToe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ticTacToe
+//
+//  Created by Travis Moody on 6/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
